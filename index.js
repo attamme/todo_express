bd77eb8b7a5f8e48397d5ccb1599d433597d080e
Annaliisa Tamme <annaliisa.tamme@voco.ee>
2024-09-16 11:29:27 +0300

    Write data to file

--- index.js
+++ index.js
@@ -7,17 +7,49 @@ const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res) => {
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => {
     // get data from file
-    fs.readFile('./tasks', 'utf8', (err, data) => {
+    fs.readFile(filename, 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             return;
         }
         // tasks list data from file
         const tasks = data.split("\n") 
+        resolve(tasks)
+        });
+    })
+}
+
+app.get('/', (req, res) => {
+    // tasks list data from file
+    readFile('./tasks')
+    .then(tasks => {
+        console.log(tasks)
         res.render('index', {tasks: tasks})
-    });
+    })
+})
+
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+    // tasks list data from file
+    readFile('./tasks')
+    .then(tasks =>{
+        // add form sent task to tasks array
+        tasks.push(req.body.task)
+        const data = tasks.join("\n") 
+        fs.writeFile('./tasks', data, err => {
+            if (err) {
+                console.error(err);
+                return;
+            } 
+            // redirect to / to see results
+            res.redirect('/')
+        })
+    }) 
 })
 
 app.listen(3001, () => {
